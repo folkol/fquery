80d2719214d554fbb0700c653413bc6648ab82d2
Matte <mattias.johansson@funnel.io>
2024-02-24 22:29:55 +0100

    Added ArrowTypes map

---
+++ index.js
@@ -0,0 +1,17 @@
+import * as Arrow from "apache-arrow";
+
+let ArrowTypes = {
+    BooleanType: Arrow.Type.Bool,
+    Int8Type: Arrow.Type.Int8,
+    Int16Type: Arrow.Type.Int16,
+    Int32Type: Arrow.Type.Int32,
+    Int64Type: Arrow.Type.Int64,
+    UInt8Type: Arrow.Type.Uint8,
+    UInt16Type: Arrow.Type.Uint16,
+    UInt32Type: Arrow.Type.Uint32,
+    UInt64Type: Arrow.Type.Uint64,
+    FloatType: Arrow.Type.Float32,
+    DoubleType: Arrow.Type.Float64,
+    StringType: Arrow.Type.Utf8,
+}
+
